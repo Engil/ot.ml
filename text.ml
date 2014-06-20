b38959edd0629a0b3afef2571b223f2b315673f2
Enguerrand Decorne <decorne.en@gmail.com>
2014-06-20 14:55:52 +0200

    Diff is now working

--- text.ml
+++ text.ml
@@ -7,9 +7,31 @@ let explode s =
 let concat s =
   List.fold_left (fun str elt -> str ^ elt) ""
 
+let rec retaini ops = function
+  | 0 -> ops
+  | i -> retaini (Ot.RetainOp (ops)) (i - 1)
+
+let rec deletei ops = function
+  | 0 -> ops
+  | i -> deletei (Ot.DeleteOp (ops)) (i - 1)
+
+let rec addi str ops = function
+  | 0 -> ops
+  | i -> addi str (Ot.InsertOp ((String.get str (i - 1)), ops)) (i - 1)
+
+let (<|) x f = f x
+let flip f a x = f x a
+
 let diffs_of_texts t1 t2 =
   let l1 = String.length t1 in
   let l2 = String.length t2 in
+  let compute_ots (endl, endr) (beginl, beginr)  =
+    let to_add = String.sub t2 (beginr) (endr - beginr + 1) in
+    retaini Ot.EmptyOp (l1 - endl - 1) <|
+    flip (addi to_add) (endr - beginr + 1) <|
+    flip deletei (endl - beginl + 1) <|
+    flip retaini beginl
+  in
   let rec iter_reverse = function
     | -1, -1 -> None
     | -1, i -> Some (0, i)
@@ -29,15 +51,15 @@ let diffs_of_texts t1 t2 =
         iter (succ left, succ right)
     else
     if right >= l2 then
-        Some (left, right)
-      else
-      if (String.get t1 left) != (String.get t2 right) then
-        Some (left, right)
-      else
-        iter (succ left, succ right) in
-  let finish = iter_reverse (l1, l2) in
+      Some (left, right)
+    else
+    if (String.get t1 left) != (String.get t2 right) then
+      Some (left, right)
+    else
+      iter (succ left, succ right) in
+  let finish = iter_reverse (l1 - 1, l2 - 1) in
   let begining = iter (0, 0) in
   match finish, begining with
   | None, None-> None
-  | Some l, Some r -> Some (l, r)
+  | Some d1, Some d2 -> Some (compute_ots d1 d2)
   | _ -> None
