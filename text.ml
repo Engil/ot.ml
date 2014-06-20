e9be6f14523bbf38d35da99cccada31949873cf8
Enguerrand Decorne <decorne.en@gmail.com>
2014-06-20 15:00:28 +0200

    And it seems that <| was in fact |>, much surprise.

--- text.ml
+++ text.ml
@@ -19,7 +19,7 @@ let rec addi str ops = function
   | 0 -> ops
   | i -> addi str (Ot.InsertOp ((String.get str (i - 1)), ops)) (i - 1)
 
-let (<|) x f = f x
+let (|>) x f = f x
 let flip f a x = f x a
 
 let diffs_of_texts t1 t2 =
@@ -27,9 +27,9 @@ let diffs_of_texts t1 t2 =
   let l2 = String.length t2 in
   let compute_ots (endl, endr) (beginl, beginr)  =
     let to_add = String.sub t2 (beginr) (endr - beginr + 1) in
-    retaini Ot.EmptyOp (l1 - endl - 1) <|
-    flip (addi to_add) (endr - beginr + 1) <|
-    flip deletei (endl - beginl + 1) <|
+    retaini Ot.EmptyOp (l1 - endl - 1) |>
+    flip (addi to_add) (endr - beginr + 1) |>
+    flip deletei (endl - beginl + 1) |>
     flip retaini beginl
   in
   let rec iter_reverse = function
